be5cd083720d9b47f4004e66d83dea541a2416c6
mojtabangh <mojtaba83ngh@gmail.com>
2022-11-15 16:47:34 +0330

    CKeditor was installed

--- src/config/settings.py
+++ src/config/settings.py
@@ -132,3 +132,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CKeditor config
+CKEDITOR_UPLOAD_PATH = 'media/uploads/'
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'full',
+    },
+}
