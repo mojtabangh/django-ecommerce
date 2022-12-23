329756b0ab3a066b5c868b655737077b8240ef1b
mojtabangh <mojtaba83ngh@gmail.com>
2022-12-23 13:47:06 +0330

    order_create view was edited and order_i session was added

--- src/orders/views.py
+++ src/orders/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from orders.models import OrderItem
 from orders.forms import OrderCreateForm
@@ -22,9 +23,10 @@ def order_create(request):
             cart.clear()
             # launch asynchronous task
             order_created.delay(order.id)
-            return render(request,
-            'orders/order/created.html',
-            {'order': order})
+            # Set the order in the session
+            request.session['order_id'] = order.id
+            # Redirect for payment
+            return redirect(reverse('payment:process'))
     else:
         form = OrderCreateForm()
     return render(request,
