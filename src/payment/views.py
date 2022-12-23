c4877034e3f487c8e6a396364e58e125c7df78ac
mojtabangh <mojtaba83ngh@gmail.com>
2022-12-23 13:48:11 +0330

    payment views were written

--- src/payment/views.py
+++ src/payment/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+import braintree
+
+from django.shortcuts import render, redirect, get_object_or_404
+from django.conf import settings
+
+from orders.models import Order
 
 # Create your views here.
+
+# Instantiate Braintree payment gateway
+gateway = braintree.BraintreeGateway(settings.BRAINTREE_CONF)
+
+def payment_process(request):
+    order_id = request.session.get('order_id')
+    order = get_object_or_404(Order, id=order_id)
+    total_cost = order.get_total_cost()
+    if request.method == 'POST':
+        nonce = request.POST.get('payment_method_nonce', None)
+        # Create and submit transaction
+        result = gateway.transaction.sale({
+            'amount': f'{total_cost:.2f}',
+            'payment_method_nonce': nonce,
+            'options': {
+                'submit_for_settlement': True,
+            }
+        })
+        if result.is_success:
+            # Mark the order as paid
+            order.paid = True
+            # Store the unique transaction id
+            order.braintree_id = result.transaction.id
+            order.save()
+            return redirect('payment:done')
+        else:
+            # Generate token
+            client_token = gateway.client_token.generate()
+            return render(
+                request,
+                'payment/process.html',
+                {'order': order,
+                'client_token': client_token}
+            )
