c1f67a4aae5a1dcb65c28e94233ad721c72b27c0
mojtabangh <mojtaba83ngh@gmail.com>
2022-12-23 18:35:35 +0330

    views for payment and callback were written

--- src/payment/views.py
+++ src/payment/views.py
@@ -15,13 +15,14 @@ def payment_process(request):
     order = get_object_or_404(Order, id=order_id)
     total_cost = order.get_total_cost()
     if request.method == 'POST':
+        # retrieve nonce
         nonce = request.POST.get('payment_method_nonce', None)
         # Create and submit transaction
         result = gateway.transaction.sale({
             'amount': f'{total_cost:.2f}',
             'payment_method_nonce': nonce,
             'options': {
-                'submit_for_settlement': True,
+                'submit_for_settlement': True
             }
         })
         if result.is_success:
@@ -32,14 +33,14 @@ def payment_process(request):
             order.save()
             return redirect('payment:done')
         else:
-            # Generate token
-            client_token = gateway.client_token.generate()
-            return render(
-                request,
-                'payment/process.html',
-                {'order': order,
-                'client_token': client_token}
-            )
+            return redirect('payment:canceled')
+    else:
+        # Generate token
+        client_token = gateway.client_token.generate()
+        return render(request,
+            'payment/process.html',
+            {'order': order,
+            'client_token': client_token})
 
 def payment_done(request):
     return render(request, 'payment/done.html')
