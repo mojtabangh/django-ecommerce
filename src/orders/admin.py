d634c9b5c1388cf9ab8ac73f58c55318ebd0847c
mojtabangh <mojtaba83ngh@gmail.com>
2022-12-23 20:12:00 +0330

    admin_order_detail was written and added to django administrator

--- src/orders/admin.py
+++ src/orders/admin.py
@@ -1,6 +1,8 @@
 import csv
 import datetime
 
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 from django.http import HttpResponse
 from django.contrib import admin
 from orders.models import Order, OrderItem
@@ -11,6 +13,9 @@ class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product',]
 
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
 
 def export_to_csv(modeladmin, request, queryset):
     opts = modeladmin.model._meta
@@ -39,7 +44,7 @@ export_to_csv.short_description = 'Export to CSV'
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
     'address', 'postal_code', 'city', 'paid',
-    'created', 'updated']
+    'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
