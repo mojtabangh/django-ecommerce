5ffd50a2de6ddd91173c448aff77748321a423b5
mojtabangh <mojtaba83ngh@gmail.com>
2022-12-29 14:07:34 +0330

    the URLs were updated for CBV

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from shop.views import product_list, product_detail
+from shop.views import product_list, product_detail, ProductListView, ProductDetailView
 
 app_name = 'shop'
 urlpatterns = [
-    path('', product_list, name='product_list'),
-    path('<slug:category_slug>/', product_list, name='product_list_by_category'),
-    path('<int:id>/<slug:slug>/', product_detail, name='product_detail'),
+    path('', ProductListView.as_view(), name='product_list'),
+    path('<slug:category_slug>/', ProductListView.as_view(), name='product_list_by_category'),
+    path('<int:id>/<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
 ]
