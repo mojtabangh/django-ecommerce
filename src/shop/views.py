b2cc95ae8413ab68a7fa177cdce5b6d2f6854d7e
mojtabangh <mojtaba83ngh@gmail.com>
2022-12-30 10:56:19 +0330

    the files were sorted and the redundant codes were deleted

--- src/shop/views.py
+++ src/shop/views.py
@@ -1,21 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import View, DetailView
+
 from shop.models import Product, Category
 from cart.forms import CartAddProductForm
-# Create your views here.
-
-def product_list(request, category_slug=None):
-    category = None
-    categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
-    if category_slug:
-        category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
-    return render(request,
-        'shop/product/list.html',
-        {'category': category,
-        'categories': categories,
-        'products': products})
 
 class ProductListView(View):
     def get(self, request, *args, **kwargs):
@@ -30,16 +17,6 @@ class ProductListView(View):
             'categories': Category.objects.all(),
         }
         return render(request, 'shop/product/list.html', context)
-
-
-def product_detail(request, id, slug):
-    product = get_object_or_404(Product, id=id,
-    slug=slug,available=True)
-    cart_product_form = CartAddProductForm()
-    return render(request,
-    'shop/product/detail.html',
-    {'product': product,
-    'cart_product_form': cart_product_form,})
 
 
 class ProductDetailView(DetailView):
