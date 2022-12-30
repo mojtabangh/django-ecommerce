ef2557e846d57ff871ea47b298887382d8851201
mojtabangh <mojtaba83ngh@gmail.com>
2022-12-30 12:11:31 +0330

    the redundant codes were deleted

--- src/orders/views.py
+++ src/orders/views.py
@@ -46,35 +46,6 @@ class OrderCreateView(View):
             return redirect(reverse('payment:process'))
 
 
-def order_create(request):
-    cart = Cart(request)
-    if request.method == 'POST':
-        form = OrderCreateForm(request.POST)
-        if form.is_valid():
-            order = form.save(commit=False)
-            if cart.coupon:
-                order.coupon = cart.coupon
-                order.discount = cart.coupon.discount
-            order.save()
-            for item in cart:
-                OrderItem.objects.create(order=order,
-                product=item['product'],
-                price=item['price'],
-                quantity=item['quantity'],)
-            # clear the cart
-            cart.clear()
-            # launch asynchronous task
-            order_created.delay(order.id)
-            # Set the order in the session
-            request.session['order_id'] = order.id
-            # Redirect for payment
-            return redirect(reverse('payment:process'))
-    else:
-        form = OrderCreateForm()
-    return render(request,
-    'orders/order/create.html',
-    {'cart': cart, 'form': form})
-
 @staff_member_required
 def admin_order_detail(request, order_id):
     order = get_object_or_404(Order, id=order_id)
