2fc836b4101b2f4447c6d43f61af53b20bc6a7d4
mojtabangh <mojtaba83ngh@gmail.com>
2022-12-30 18:52:33 +0330

    cart add, remove and detail view were replaced with CBV

--- src/cart/views.py
+++ src/cart/views.py
@@ -1,36 +1,47 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.decorators.http import require_POST
+from django.views.generic import View, TemplateView
 
 from shop.models import Product
 from cart.cart import Cart
 from cart.forms import CartAddProductForm
 from coupons.forms import CouponApplyForm
 
-# Create your views here.
+class CartAddView(View):
+    def post(self, request, *args, **kwargs):
+        cart = Cart(self.request)
+        product = get_object_or_404(Product, id=self.kwargs['product_id'])
+        form = CartAddProductForm(self.request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            cart.add(
+                product=product,
+                quantity=cd['quantity'],
+                override_quantity=cd['override']
+            )
+        return redirect('cart:cart_detail')
 
-@require_POST
-def cart_add(request, product_id):
-    cart = Cart(request)
-    product = get_object_or_404(Product, id=product_id)
-    form = CartAddProductForm(request.POST)
-    if form.is_valid():
-        cd = form.cleaned_data
-        cart.add(product=product,
-        quantity=cd['quantity'],
-        override_quantity=cd['override'])
-    return redirect('cart:cart_detail')
-    
-def cart_remove(request, product_id):
-    cart = Cart(request)
-    product = get_object_or_404(Product, id=product_id)
-    cart.remove(product)
-    return redirect('cart:cart_detail')
 
-def cart_detail(request):
-    cart = Cart(request)
-    for item in cart:
-        item['update_quantity_form'] = CartAddProductForm(initial={
-            'quantity': item['quantity'],
-            'override': True})
-    coupon_apply_form = CouponApplyForm()
-    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form})
+class CartRemoveView(View):
+    def post(self, request, *args, **kwargs):
+        cart = Cart(self.request)
+        product = get_object_or_404(Product, id=self.kwargs['product_id'])
+        cart.remove(product)
+        return redirect('cart:cart_detail')
+
+
+class CartDetailView(TemplateView):
+    template_name = 'cart/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CartDetailView, self).get_context_data(**kwargs)
+        cart = Cart(self.request)
+        for item in cart:
+            item['update_quantity_form'] = CartAddProductForm(
+                initial={
+                    'quantity': item['quantity'],
+                    'override': True
+                }
+            )
+        context['cart'] = cart
+        context['coupon_apply_form'] = CouponApplyForm()
+        return context
